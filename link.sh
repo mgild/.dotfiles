b05025ea4b819df1fdbbba0ce4de5b3015fc0bd6
Mitchell <mgild@umich.edu>
2016-10-06 19:58:10 -0400

    moved oh-my-zsh out

--- link.sh
+++ link.sh
@@ -1,4 +1,6 @@
 ln -s ~/.dotfiles/.zshrc ~/
 ln -s ~/.dotfiles/.vimrc ~/
-ln -s ~/.dotfiles/.oh-my-zsh ~/
+if [ ! -d ~/.oh-my-zsh]; then
+    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
 
