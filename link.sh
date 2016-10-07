68f3fa5cefedc19400ac476d396604b35cb4d126
Mitchell <mgild@umich.edu>
2016-10-06 20:29:46 -0400

    Vundle added

--- link.sh
+++ link.sh
@@ -5,6 +5,11 @@ if [ ! -e "/Users/$(whoami)/.oh-my-zsh/" ]
 then
     sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
+# install Vundle
+if [ ! -e " ~/.vim/bundle/Vundle.vim" ]
+then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 # install vim pluigns
 vim -es -c "PluginInstall" -c "q" 
 
