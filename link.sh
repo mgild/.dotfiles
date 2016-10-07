e86edf711402d1b538aa28d6aa5a13588aa07e08
Mitchell <mgild@umich.edu>
2016-10-07 10:37:08 -0400

    fixed oh-my-zsh initial install

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
-#! /bin/zsh
-ln -s ~/.dotfiles/.zshrc ~/
-ln -s ~/.dotfiles/.vimrc ~/
+#! /bin/bash
+CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)" 
+ln -s $CWD/.vimrc ~/
 # install Vundle
 if [[ ! -a "${HOME}/.vim/bundle/Vundle.vim" ]]
 then
@@ -14,6 +14,11 @@ ln -s ~/.vim/bundle/vim-colorschemes/colors ~/.vim/colors
 # install oh-my-zsh
 if [[ ! -a "${HOME}/.oh-my-zsh" ]]
 then
-    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed "s/env zsh//g")"
+    # stop oh-my-zsh from auto zsh overwrite
+    rm ~/.zshrc
 fi
+echo "linking $CWD/.zshrc"
+ln -s $CWD/.zshrc ~/
+
 
