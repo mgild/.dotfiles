8865dbb4c0104269fb2ecc8577eb0a1eaade3842
Mitchell <mgild@umich.edu>
2017-08-24 13:40:21 -0700

    readding brew check for linux

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -1,2 +1,4 @@
+if ! which brew &> /dev/null; then
     # unbiquitous non-root package manager for linux distros
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+fi
