7e79f2d0835992aaf568e91999a2d41f9c6ba2bf
Mitchell <mgild@umich.edu>
2017-08-24 13:25:30 -0700

    figuring out bug

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -1,4 +1,2 @@
-if ! which brew &> /dev/null; then
     # unbiquitous non-root package manager for linux distros
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-fi
