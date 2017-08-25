d77f97849a442e75afa51a8fa4c439026d4210c3
Mitchell <mgild@umich.edu>
2017-08-25 10:08:15 -0700

    changed ordering to get rid of path warning

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -1,7 +1,7 @@
 if ! which brew &> /dev/null; then
+    PATH="$PATH:/home/${USER}/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/bin"
     # unbiquitous non-root package manager for linux distros
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-    PATH="$PATH:/home/${USER}/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/bin"
 fi
 
 if [[ ! -a "/home/${USER}/python-dev/include/python2.7/Python.h" ]]; then
