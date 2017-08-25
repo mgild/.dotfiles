42b9408700d425d7cf1aee6edf3cabbccbd0d05c
Mitchell <mgild@umich.edu>
2017-08-25 13:12:48 -0700

    moved python dev building to temp

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -6,10 +6,9 @@ if [[ ! -a "/home/${USER}/.linuxbrew/bin/brew" && \
 fi
 
 if [[ ! -a "/home/${USER}/python-dev/include/python2.7/Python.h" ]]; then
-    wget "https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz" -O ~/python-dev.tgz
-    tar -zxvf ~/python-dev.tgz -C ~/
-    cd ~/Python-2.7.13/
-    ./configure --enable-shared --enable-unicode --prefix=/home/${USER}/python-dev
-    make && make install
-    rm -rf ~/Python-2.7.13/
+    wget "https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz" -O /tmp/python-dev.tgz
+    tar -zxvf /tmp/python-dev.tgz -C /tmp/
+    /tmp/Python-2.7.13/configure --enable-shared --enable-unicode --prefix=/home/${USER}/python-dev
+    /tmp/Python-2.7.13/make && /tmp/Python-2.7.13/make install
+    rm -rf /tmp/Python-2.7.13/
 fi
