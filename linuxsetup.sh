57c969e396e7a9941dad2c10396402a26ac76aa2
Mitchell <mgild@umich.edu>
2017-08-25 15:48:08 +0000

    python-dev build fixes

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -3,11 +3,11 @@ if ! which brew &> /dev/null; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 fi
 
-if ! -d "~/python-dev"; then
+if [[ ! -a "/home/${USER}/python-dev/include/python2.7/Python.h" ]]; then
     wget "https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz" -O ~/python-dev.tgz
-    tar xvf ~/python-dev.tgz
+    tar -zxvf ~/python-dev.tgz -C ~/
     cd ~/Python-2.7.13/
-    ./configure --enable-shared --enable-unicode --prefix=~/python-dev
+    ./configure --enable-shared --enable-unicode --prefix=/home/${USER}/python-dev
     make && make install
 fi
 
