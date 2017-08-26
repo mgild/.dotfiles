50a73973e1b25ccd449af214c4e03c73c92554c9
Mitchell <mgild@umich.edu>
2017-08-25 18:52:22 -0700

    removing ohmyzsh auto move zshrc line

--- ohmyzsh-setup.sh
+++ ohmyzsh-setup.sh
@@ -1,6 +1,9 @@
 # install oh-my-zsh
 if [[ ! -a "${HOME}/.oh-my-zsh" ]]; then
-    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sed 's/env zsh//g')"
+    # excludes ohmyzsh's default zshrc and prevents entering zshell on script finish
+    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - \
+        | sed 's/env zsh//g'\
+        | sed 's;mv -f ~/.zshrc-omztemp ~/.zshrc;;g')"
     # Remove the newly created zshrc
     rm ~/.zshrc
 fi
