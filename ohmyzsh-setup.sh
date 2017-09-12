0588415371a1c5bec38f4a9bdeb2d8d7aa5ebc28
Mitchell <mgild@umich.edu>
2017-09-12 01:17:16 -0400

    switching to custom ohmyzsh build script

--- ohmyzsh-setup.sh
+++ ohmyzsh-setup.sh
@@ -1,8 +1,4 @@
 # install oh-my-zsh
 if [[ ! -a "${HOME}/.oh-my-zsh" ]]; then
-    # excludes ohmyzsh's default zshrc and prevents entering zshell on script finish
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh \
-        | sed 's/env zsh//g'\
-        | sed 's;cp $ZSH/templates/zshrc.zsh-template ~/.zshrc;;g' \
-        | sed 's;mv -f ~/.zshrc-omztemp ~/.zshrc;;g')"
+    . "$CWD/ohmyzsh_build.sh"
 fi
